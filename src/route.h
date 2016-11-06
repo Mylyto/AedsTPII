aa108f23e579e0209aa822d9aebff63a7ca615e8
primary157 <victorgvbh@gmail.com>
2016-11-06 00:38:30 -0200

    Voltando a Compilar...

--- src/route.h
+++ src/route.h
@@ -6,25 +6,25 @@
 
 //A rota possui um vetor de cidades pela a qual ela percorre
 typedef struct{
-    TCity cities[MAX];
+    City cities[MAX];
     unsigned int endDemand; // demanda total da rota
     unsigned int topo; // num de cidades na rota
     unsigned int theSize; // o tamanho da rota
-}TRoute;
+}Route;
 
 // Lista de rotas
 // IDEIA: 4 cidades e 2 caminhoẽs, uma rota seria 010, outra 02340.
 // Loga essa lista de rotas seria guardada na lista. e cria outra lista para as rotas 0120 0340
 typedef struct{
-    TRoute route[MAX];
+    Route route[MAX];
     unsigned int endSize; // tamanho total de toda a rota
     unsigned int topo; // numero de rotas;
-}TListRoute;
+}ListRoute;
 
 
-void initRoute(TRoute* route, unsigned int num_city);
-void initListRoute(TListRoute* listRoute);
-int insertListRoute(TListRoute* listRoute, TRoute* route);
-int insertRouteCity(TRoute* route, TCity* city, unsigned int distance);
+void initRoute(Route* route, unsigned int num_city);
+void initListRoute(ListRoute* listRoute);
+int insertListRoute(ListRoute* listRoute, Route* route);
+int insertRouteCity(Route* route, City* city, unsigned int distance);
 
 #endif // ROUTE_H_INCLUDED
